1a157af52b2e4a2e2abf146f0961f5ac28a8645a
vishal <vishal_s_yadav@yahoo.com>
2013-10-03 23:35:59 -0700

    REDO ALL THAT BECAUSE THIS IS MUCH BETTER

---
+++ read-command.c
@@ -0,0 +1,91 @@
+// UCLA CS 111 Lab 1 command reading
+
+#include "command.h"
+#include "command-internals.h"
+#include "alloc.h"
+
+#include <error.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/* FIXME: You may need to add #include directives, macro definitions,
+   static function definitions, etc.  */
+
+/* FIXME: Define the type 'struct command_stream' here.  This should
+   complete the incomplete type declaration in command.h.  */
+
+struct command_stream {
+  char x;
+  struct command_stream *next;
+  struct command_stream *previous;
+};
+
+command_stream_t
+make_command_stream (int (*get_next_byte) (void *),
+		     void *get_next_byte_argument)
+{
+  /* FIXME: Replace this with your implementation.  You may need to
+     add auxiliary functions and otherwise modify the source code.
+     You can also use external functions defined in the GNU C Library.  */
+    struct command_stream *tail;
+    tail = checked_malloc(sizeof(struct command_stream));
+    tail->next = 0;
+    tail->previous = 0;
+    tail->x = '\0';
+    int newline_spotted = 0;
+    char curr_byte;
+    struct command_stream *nextChar;
+
+    curr_byte = get_next_byte(get_next_byte_argument);
+    if(curr_byte == EOF) {
+        return tail;
+    } else {
+        tail -> x = curr_byte;
+    }
+
+    while((curr_byte = get_next_byte(get_next_byte_argument)) != EOF) {
+        if(curr_byte == '\n') {
+            newline_spotted = 1;
+            continue;
+        }
+        if(newline_spotted == 1) {
+            newline_spotted = 0;
+            tail -> next = checked_malloc(sizeof(struct command_stream));
+            nextChar = tail -> next;
+            nextChar -> previous = tail;
+            nextChar -> next = 0;
+            nextChar -> x = '\n';
+            tail = nextChar;
+            nextChar = 0;
+        } else {
+            if(curr_byte == '#') {
+                while((curr_byte = get_next_byte(get_next_byte_argument)) != EOF) {
+                    if(curr_byte == '\n') {
+                        break;
+                    }
+                }
+                continue;
+            }
+        }
+        tail -> next = checked_malloc(sizeof(struct command_stream));
+        nextChar = tail -> next;
+        nextChar -> previous = tail;
+        nextChar -> next = 0;
+        nextChar -> x = curr_byte;
+        tail = nextChar;
+        nextChar = 0;
+        newline_spotted = 0;
+    }
+
+    return tail;
+}
+
+command_t
+read_command_stream (command_stream_t s)
+{
+  /* FIXME: Replace this with your implementation too.  */
+  error (1, 0, "command reading not yet implemented");
+  return 0;
+}
