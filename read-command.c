c2ec663dbfbf4acc068f45cad670633fbca6352d
Sean Grant <sgrant24@ucla.edu>
2013-10-04 01:29:24 -0700

    read command on the right track (I hope)

--- read-command.c
+++ read-command.c
@@ -82,10 +82,154 @@ make_command_stream (int (*get_next_byte) (void *),
     return tail;
 }
 
+
+/*
 command_t
 read_command_stream (command_stream_t s)
 {
-  /* FIXME: Replace this with your implementation too.  */
-  error (1, 0, "command reading not yet implemented");
+	struct command_stream curr = s;
+	int inParen = 0; 
+        while (curr != 0)
+        {
+                if (curr->x == ')')
+		{
+			inParen++;
+			continue;
+		}
+		else if (inParen>0 && curr->x != '(')
+		{
+			continue;
+		}
+		else if (inParen > 0 && curr->x == '(')
+		{
+			inParen--;
+			continue;
+		}
+		else if (curr->x == '&' && curr->previous->x == '&')
+		{
+			curr->next->previous = 0;
+			return rcsHelp (curr->previous, s, AND_COMMAND);
+		}
+		else if (curr ->x == '|' && curr->previous->x = '|')
+		{
+                        curr->next->previous = 0;
+                        return rcsHelp (curr->previous, s, OR_COMMAND);
+
+		}
+		else if (curr->x == ';' || curr->x == '\n')
+		{
+                        curr->next->previous = 0;
+                        return rcsHelp (curr->previous, s, SEQUENCE_COMMAND);
+		}
+		else
+		{
+			curr = curr->previous;
+		}
+        }
+
+  //error (1, 0, "command reading not yet implemented");
   return 0;
 }
+
+*/
+
+command_t
+read_command_stream (command_stream_t s)
+{
+        struct command_stream curr = s;
+        int inParen = 0;
+        while (curr != 0)
+        {
+                if (curr->x == ')')
+                {
+                        inParen++;
+                        continue;
+                }
+                else if (inParen>0 && curr->x != '(')
+                {
+                        continue;
+                }
+                else if (inParen > 0 && curr->x == '(')
+                {
+                        inParen--;
+                        continue;
+                }
+                else if (curr->x == '&' && curr->previous->x == '&')
+                {
+                        curr->next->previous = 0;
+                        return rcsHelp (curr->previous, s, AND_COMMAND);
+                }
+                else if (curr ->x == '|' && curr->previous->x = '|')
+                {
+                        curr->next->previous = 0;
+                        return rcsHelp (curr->previous, s, OR_COMMAND);
+
+                }
+                else if (curr->x == ';' || curr->x == '\n')
+                {
+                        curr->next->previous = 0;
+                        return rcsHelp (curr->previous, s, SEQUENCE_COMMAND);
+                }
+                else
+                {
+                        curr = curr->previous;
+                }
+        }
+	curr = s;
+	while (curr != 0)
+	{
+                if (curr->x == ')')
+                {
+                        inParen++;
+			continue;
+                }
+                else if (inParen>0 && curr->x != '(')
+                {
+                        continue;
+                }
+                else if (inParen > 0 && curr->x == '(')
+                {
+                        inParen--;
+                        continue;
+                }
+		else if (curr->x == '|')
+		{
+                        curr->next->previous = 0;
+                        return rcsHelp (curr->previous, s, PIPE_COMMAND);
+		}
+		else
+		{
+			curr = curr->previous;
+		}
+	}
+	curr = s;
+	while (curr != 0)
+	{
+                if (curr->x == ')')
+                {
+                        curr = curr->previous;
+			struct command_stream parenTail = curr;
+                        while (curr->previous != '(')
+                        {
+                                curr = curr->previous;
+                                continue;
+                        }
+			curr->previous = 0;
+			return read_command_stream (parenTail);
+                }
+		else
+		{
+			curr = curr->previous
+		}
+	}
+	while (curr != 0)
+
+
+
+
+
+
+
+
+	return 0;
+} 
